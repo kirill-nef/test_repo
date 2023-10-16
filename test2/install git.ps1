67115e78863f126e393b25684a542e1ebbd41b0a
Kirill Nefedov <88302370+kirill-nef@users.noreply.github.com>
2023-10-16 15:32:10 +0500

    upload

---
+++ test2/install git.ps1
@@ -0,0 +1,115 @@
+# C:\gitlab-runner\*
+$GLPath = "$env:SystemDrive\gitlab-runner"
+$GLBin  = "$GLPath\gitlab-runner.exe"
+
+# Для любого скрипта или команды, выполняющегося вне сеанса, требуется using модификатор область для внедрения значений переменных из вызывающего сеанса область, чтобы получить к ним доступ из кода сеанса. Фоновые задания, запущенные с Start-Job (сеанс вне процесса).
+$Token  = $using:Token
+$User   = $using:User
+$Pass   = $using:Pass
+
+
+$GitUri = 'https://github.com/git-for-windows/git/releases/download/v2.25.0.windows.1/Git-2.25.0-64-bit.exe'
+$Uri    = 'https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-windows-amd64.exe'
+
+# При возникновении ошибок - молча продолжать
+$progressPreference = 'silentlyContinue'
+
+# Указываем типы протоколов безопасности
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
+
+
+# Устновка GIT
+& git --version | Out-Null
+if ($LASTEXITCODE -ne 0) {
+    Set-Location "$env:windir\temp"
+    Invoke-WebRequest -UseBasicParsing -Uri $GitUri -OutFile ".\git-installer.exe"
+    Start-Process '.\git-installer.exe' -ArgumentList '/SILENT' -NoNewWindow -Wait
+    Remove-Item '.\git-installer.exe' -Force
+    # Обновление переменных
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+}
+
+# Настройка GIT
+# -ea 0 (error action) - ошибка не выведется на экран - работа продолжится
+# Тут используется обновление gitlab-runner
+if(Get-Service "gitlab-runner" -ea 0){
+    # получим полный путь до исполняемого файла, split - разбить по строка, Select-Object -First 1 - взять первый элемент из всех строк
+    $bin = (Get-WmiObject win32_service | Where-Object{$_.Name -like 'gitlab-runner'} | Select-Object PathName).PathName -split ' ' | Select-Object -First 1
+    # получаем папку где лежит исполняемый файл
+    $GLPath = Split-Path $bin -Parent
+    Set-Location $GLPath
+    # Узнаем версию исполняемого файла, c помомщью рег выр -match ищем номер версии, Out-Null скроет значение
+    & $bin --version | Where-Object {$_ -match '^Version:\s*(\d+\.\d+\.\d+)'} | Out-Null
+    # Получем результат в виде - Major, minor, build
+    $VersionOld = [version]$Matches[1]
+    # если значение отстутсвует, выдать ошибку и завершить работу скрипта с кодом 1
+    if(!$VersionOld) {
+        throw "Error"
+        exit 1
+    }
+
+    $GLBinNew = "$GLPath\gitlab-runner_new.exe"
+    # качаем новый файл
+    Invoke-WebRequest -UseBasicParsing -Uri $Uri -OutFile $GLBinNew
+
+    # проверяем версию
+    & $GLBinNew --version | Where-Object {$_ -match '^Version:\s*(\d+\.\d+\.\d+)'} | Out-Null
+    $VersionNew = [version]$Matches[1]
+
+    # сравнение, если больше или равно
+    if($VersionOld -ge $VersionNew){
+        # удаляем и завершаем работу скрипта
+        Remove-Item $GLBinNew
+        exit 0
+    }
+
+    # если версия новая, тогда стопаем, удаляем, и удаляем исполняемый файл
+    Write-Host "`nУстановка версии $VersionNew"
+    & $bin stop      2>&1 | Write-Host
+    & $bin uninstall 2>&1 | Write-Host
+    Remove-Item $bin -Force
+
+    # переименовываем gitlab-runner_new.exe в gitlab-runner.exe
+    Rename-Item $GLBinNew -NewName $GLBin
+    # Проводим установук с параметрами
+    & $GLBin install --user "$User" --password "$Pass" 2>&1 | Write-Host
+    & $GLBin start 2>&1 | Write-Host
+
+    Write-Host "`nDone."
+}
+# Первоначальная установка
+else{
+    # Создаем директорию
+    New-Item -ItemType Directory -Path $GLPath -ea 0
+    Set-Location $GLPath
+
+    # получаем имя своего компа, если оно не равно dev.kontur, тогда отдаем $false='Evrika-prod'
+    $Tags = @{$true='Evrika-dev';$false='Evrika-prod'}[([System.Net.Dns]::GetHostByName("localhost").HostName) -match 'dev.kontur']
+
+    # качаем исполняемый файл
+    Invoke-WebRequest -UseBasicParsing -Uri $Uri -OutFile $GLBin
+
+    # устанавливаем
+    & $GLBin install --user "$User" --password "$Pass" 2>&1 | Write-Host
+
+    # меняем политики безопасности, а именно пользователя, gc=getcontent,sc=select content
+    $tmp = New-TemporaryFile
+    secedit /export /cfg "$tmp.inf" | Out-Null
+    (gc -Encoding ascii "$tmp.inf") -replace '^SeServiceLogonRight .+', "`$0,$User" | sc -Encoding ascii "$tmp.inf"
+    secedit /import /cfg "$tmp.inf" /db "$tmp.sdb" | Out-Null
+    secedit /configure /db "$tmp.sdb" /cfg "$tmp.inf" | Out-Null
+    rm $tmp* -ea 0
+
+    # регистрируемся на git
+    & $GLBin register                     `
+        --non-interactive                 `
+        --url 'https://git.skbkontur.ru/' `
+        --registration-token $Token       `
+        --executor "shell"                `
+        --description "Evrika"            `
+        --tag-list $Tags 2>&1 | Write-Host
+
+    # Запуск
+    Write-Host "`nStart gitlab-runner"
+    & $GLBin start 2>&1 | Write-Host
+}
---
+++ test3/install_iis.ps1
@@ -0,0 +1,97 @@
+$site_name = "mysite"
+$site_port = 4321
+$site_path = "$env:SystemDrive\$site_name"
+
+# Функция проверки возможности установки WindowsFeature
+function fn_check_module_wf {
+    if (Get-Command -Noun 'WindowsFeature') {
+        Write-Host "Нахожу модуль WindowsFeature, работа с IIS через PowerShell возможна!" -ForegroundColor Green
+        return $true
+    }
+    else {
+        Write-Host "Не найден модуль WindowsFeature. Твоя версия Windows не поддерживает способ установки через PowerShell." -ForegroundColor Red
+        return $false
+    }
+}
+
+# Функция проверки наличия установленного IIS
+function fn_check_install_iis {
+    Write-Host "Проверяю наличие установленного Web-Server (IIS)." -ForegroundColor Yellow
+    if (Get-WindowsFeature -Name Web-Server | Where-Object Installed) {
+        Write-Host "Web-Server (IIS) установлен." -ForegroundColor Green
+        return $true
+    }
+    else {
+        Write-Host "Web-Server (IIS) не установлен." -ForegroundColor Yellow
+        return $false
+    }
+}
+
+function fn_install_iis {
+    Write-Host "Запущена установка Web-Server (IIS)." -ForegroundColor Yellow
+    Install-WindowsFeature -name Web-Server -IncludeManagementTool
+}
+
+# Функция настройки и конфигурирования сайта
+function fn_config_iis {
+    # Актвируем модуль Webadministration
+    Import-Module Webadministration
+
+    # Проверяю наличие Default Web Site, если он есть - удаляю
+    if (Get-ChildItem -Path IIS:\Sites | findstr 'Default Web Site') {
+        Write-Host "Удаляю Default Web Site (дефолтный сайт)." -ForegroundColor Yellow
+        Remove-IISSite -Name "Default Web Site" -Confirm:$false
+        if (Get-ChildItem -Path IIS:\Sites | findstr 'Default Web Site') {
+            Write-Host "Не удалось удалить Default Web Site (дефолтный сайт)." -ForegroundColor Red
+        }
+    }
+
+    # Проверяю наличие сайта $site_name, если его нет - установлю
+    if (Get-ChildItem -Path IIS:\Sites | findstr ${site_name}) {
+        Write-Host "Сайт $site_name уже установлен." -ForegroundColor Green
+    }
+    else {
+        Write-Host "Устанавливаю сайт $site_name на порту $site_port." -ForegroundColor Yellow
+        mkdir $site_path -Force
+        Copy-Item ".\html_data\*" -Destination "$site_path" -Recurse
+        New-Item iis:\Sites\$site_name -bindings @{protocol="http";bindingInformation=":${site_port}:"} -physicalPath $site_path
+        if (Get-ChildItem -Path IIS:\Sites | findstr ${site_name}) {
+            Write-Host "Сайт $site_name установлен." -ForegroundColor Green 
+        }
+    }
+
+    # Выполняю конфигурацию recycle
+    Write-Host "Настраиваю Recycle." -ForegroundColor Yellow
+    set-ItemProperty 'IIS:\AppPools\DefaultAppPool' -Name Recycling.periodicRestart.time -Value 0.00:00:00
+    set-ItemProperty 'IIS:\AppPools\DefaultAppPool' -Name Recycling.periodicRestart.schedule -Value @{value="03:00:00"} 
+
+    Write-Host "Установка завершена." -ForegroundColor Green
+    exit 0
+}
+
+# Начинаем с проверки установленного модуля WindowsFeature
+if (fn_check_module_wf) {
+    # Проверяем наличие установленного IIS
+    if (fn_check_install_iis) {
+        # Вызываем функцию настройки IIS
+        fn_config_iis
+    }
+    # Если не установлен, установим и настроим
+    else {
+        # Вызываем функцию установки IIS
+        fn_install_iis
+        # Проверяем установился-ли IIS
+        if (fn_check_install_iis) {
+            # Вызываем функцию настройки IIS
+            fn_config_iis
+        }
+        else {
+            Write-Host "Установка Web-Server (IIS) не удалась." -ForegroundColor Red
+            exit 1
+        }
+    }
+}
+# Если модуль не найден, то выход
+else {
+    exit 1
+}
