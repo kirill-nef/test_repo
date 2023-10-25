8c0a882328b26b4c623393490e3d13219e47d14a
Kirill Nefedov <88302370+kirill-nef@users.noreply.github.com>
2023-10-25 14:25:56 +0500

    upd2510

--- test_kontur/Test1_server_parser.ps1
+++ test_kontur/Test1_server_parser.ps1
@@ -1,9 +1,10 @@
-# ТЗ 
-# Напиши функцию, преобразующую строку с именами серверов vm-test[1-3],vm-test3,vm-test[4-7],vm-test8 в список вида vm-test1 vm-test2 vm-test3 vm-test3 vm-test4 vm-test5 vm-test6 vm-test7 vm-test8.
-
+# Напиши функцию, преобразующую строку с именами серверов vm-test[1-3],vm-test3,vm-test[4-7],vm-test8
+# в список вида vm-test1 vm-test2 vm-test3 vm-test3 vm-test4 vm-test5 vm-test6 vm-test7 vm-test8.
 
 # Входные данные
 $in_servers_data = 'vm-test[1-3],vm-test3,vm-test[4-7],vm-test8'
+
+# Преобразуем в массив
 $in_servers_data_arr = $in_servers_data -split ','
 
 # Пустой массив для вывода
@@ -14,15 +15,13 @@ foreach ($el_servers_data_arr in $in_servers_data_arr) {
 
     # Проверяем наличие скобок
     if ($el_servers_data_arr -match [Regex]::Escape("[")) {
-
         # Отделяем имя сервера
         $serv_name = $el_servers_data_arr.Substring(0,$el_servers_data_arr.IndexOf('['))
-
-        # Получаем диапазон чисел в скобках
+        # Получаем диапазон чисел в скобках, Преобразуем [] в ! для уобства
         $servers_data_arr = $el_servers_data_arr -replace [Regex]::Escape("]"), "!" -replace [Regex]::Escape("["), "!" -match '!(.*)!'
+        # Преобразуем - в .. чтобы получился диапазон значений
         $servers_data_arr = $Matches[0] -replace [Regex]::Escape("!"), "" -replace "-", ".."
         $servers_data_arr = invoke-expression $servers_data_arr 
-
         # Какие числа есть, плюсуем в общий массив
         foreach ($elem_arr in $servers_data_arr) {
             $arr_out += $serv_name+$elem_arr
--- test_kontur/Test1_server_parser.py
+++ test_kontur/Test1_server_parser.py
@@ -1,5 +1,5 @@
-# ТЗ 
-# Напиши функцию, преобразующую строку с именами серверов vm-test[1-3],vm-test3,vm-test[4-7],vm-test8 в список вида vm-test1 vm-test2 vm-test3 vm-test3 vm-test4 vm-test5 vm-test6 vm-test7 vm-test8.
+# Напиши функцию, преобразующую строку с именами серверов vm-test[1-3],vm-test3,vm-test[4-7],vm-test8
+# в список вида vm-test1 vm-test2 vm-test3 vm-test3 vm-test4 vm-test5 vm-test6 vm-test7 vm-test8.
 
 # Вводные данные
 servers_data = 'vm-test[1-3], vm-test3, vm-test[4-7], vm-test8'
@@ -19,17 +19,16 @@ def convert_data(in_servers_data):
             serv_name_pref = elem_arr[0:elem_arr.find('[')]
             # Получаем номера сервера и создаем массив
             sev_num_pref = elem_arr[elem_arr.find('[')+1:elem_arr.find(']')].split('-')
-            # Создаем новый массив
+            # Создаем новый массив из чисел [1-4] в [1,2,3,4]
             new_sev_num_pref = []
             new_sev_num_pref.extend(range(int(sev_num_pref[0]), int(sev_num_pref[1]) + 1))
-
-            # В цикле заполняем массив выходных данных
+            # В цикле заполняем массив выходных данных (склеиваем имя сервера и номер)
             for elem in new_sev_num_pref:
                 out_data_arr.append(serv_name_pref+str(elem))
         # если элемент без скобок
         else:
             out_data_arr.append(elem_arr)
-    # Сортируем и убираем повторяющиеся значения и преобразуем в строку
-    return ', '.join(sorted(out_data_arr)).replace(',','')
+    # Сортируем и преобразуем в строку
+    return ' '.join(sorted(out_data_arr))
 
 print(convert_data(servers_data))
